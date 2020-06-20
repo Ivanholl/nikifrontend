9000b14ad7cb0106ea1aa18cf88f45c7e4f47934
Ivanholl <ivan.jelev@abv.bg>
2020-06-20 16:50:59 +0300

    added sidemenu

---
+++ src/components/SideMenu.js
@@ -0,0 +1,11 @@
+import  React, {useState} from 'react';
+
+export default function SideMenu(props) {
+    
+    return(
+        <div className={`side-menu-container ${props.openMenu ? 'opened' : ''}`}>
+            <button className="open-menu" onClick={() => props.setOpenMenu(!props.openMenu)}> {'>'} </button>
+
+        </div>
+    )
+}
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -9,33 +9,39 @@ import FifthSlide from './FifthSlide';
 
 import Navbar from '../components/Navbar';
 import BackToTopBtn from '../components/BackToTopBtn';
+import SideMenu from '../components/SideMenu';
 
 export default function Layout(props) {
     const [boxToShow, setBoxToShow] = useState(0);
+    const [openMenu, setOpenMenu] = useState(false);
 
     return (<>
+        <SideMenu openMenu={openMenu} setOpenMenu={setOpenMenu} />
         <Navbar setBoxToShow={id => setBoxToShow(id)}/>
-        <FullPage
-            initialSlide={0}
-            duration={300}
-            afterChange={args => setBoxToShow(args.to)}
-            >
-        <Slide style={{width: '100%'}}>
-            {boxToShow == 0 && <FirstSlide />}
-        </Slide>
-        <Slide style={{width: '100%'}}>
-            {boxToShow == 1 && <SecondSlide/>}
-        </Slide>
-        <Slide style={{width: '100%'}}>
-            {boxToShow == 2 && <ThirdSlide/>}
-        </Slide>
-        <Slide style={{width: '100%'}}>
-            {boxToShow == 3 && <FourthSlide/>}
-        </Slide>
-        <Slide style={{width: '100%'}}>
-            <FifthSlide animate={boxToShow == 4} />
-        </Slide>
-    </FullPage>
+        <div className={openMenu && 'fullpage'}>
+            <FullPage 
+                initialSlide={0}
+                duration={300}
+                afterChange={args => setBoxToShow(args.to)}
+                >
+                <Slide style={{width: '100%'}}>
+                    {boxToShow == 0 && <FirstSlide />}
+                </Slide>
+                <Slide style={{width: '100%'}}>
+                    {boxToShow == 1 && <SecondSlide/>}
+                </Slide>
+                <Slide style={{width: '100%'}}>
+                    {boxToShow == 2 && <ThirdSlide/>}
+                </Slide>
+                <Slide style={{width: '100%'}}>
+                    {boxToShow == 3 && <FourthSlide/>}
+                </Slide>
+                <Slide style={{width: '100%'}}>
+                    <FifthSlide animate={boxToShow == 4} />
+                </Slide>
+            </FullPage>
+        </div>
+
     <BackToTopBtn setBoxToShow={id => setBoxToShow(id)}/>
 
     </>)
